d267f09960a49d183d1f4b6c794a120801b43c9c
brafiadi <ahdiatgnr@gmail.com>
2025-01-25 10:58:04 +0700

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false, -- Muat tema saat startup
+    priority = 1000, -- Pastikan ini dimuat pertama
+    config = function()
+      require("vscode").setup({
+        transparent = false, -- Nonaktifkan latar belakang transparan jika ingin
+        italic_comments = true, -- Komentar miring
+        disable_nvimtree_bg = true, -- Hilangkan latar belakang nvim-tree
+      })
+      vim.cmd("colorscheme vscode")
+    end,
+  },
+}
