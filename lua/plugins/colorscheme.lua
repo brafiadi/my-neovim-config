f31ace8f4d9455b9940b9fd28a7f5cc1f84881ff
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2025-01-31 09:14:09 +0700

    update theme to kanagawa dragon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,27 @@
+-- return {
+--   {
+--     "Mofiqul/vscode.nvim",
+--     lazy = false, -- Muat tema saat startup
+--     priority = 1000, -- Pastikan ini dimuat pertama
+--     config = function()
+--       require("vscode").setup({
+--         transparent = false, -- Nonaktifkan latar belakang transparan jika ingin
+--         italic_comments = true, -- Komentar miring
+--         disable_nvimtree_bg = true, -- Hilangkan latar belakang nvim-tree
+--       })
+--       vim.cmd("colorscheme vscode")
+--     end,
+--   },
+-- }
+
 return {
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false, -- Muat tema saat startup
-    priority = 1000, -- Pastikan ini dimuat pertama
-    config = function()
-      require("vscode").setup({
-        transparent = false, -- Nonaktifkan latar belakang transparan jika ingin
-        italic_comments = true, -- Komentar miring
-        disable_nvimtree_bg = true, -- Hilangkan latar belakang nvim-tree
-      })
-      vim.cmd("colorscheme vscode")
-    end,
-  },
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("kanagawa").setup({
+      theme = "dragon",
+    })
+    vim.cmd("colorscheme kanagawa-dragon")
+  end,
 }
